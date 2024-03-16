7f4cd66e6450602da5fe839813768fbc55603ba6
Nubunduram <ceantoine@hotmail.fr>
2024-03-16 20:46:27 +0100

    fix: Banner Title Style

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import beachImage from '../../assets/beach.png'
 
-const TopSection = () => {
+const Banner = () => {
   return (
     <section id="banner">
       <img src={beachImage} alt="beach image" />
-      <div className='img-filter'>
+      <div className='img-filter'></div>
+      <div className='title-container'>
         <h1>Chez vous, partout et ailleurs</h1>
       </div>
     </section>
   )
 }
 
-export default TopSection
+export default Banner

0888b10b8223550cf994994a17937f72ce7d3078
Nubunduram <ceantoine@hotmail.fr>
2024-04-19 14:28:16 +0200

    fix: error page link

--- src/routes/error-page.jsx
+++ src/routes/error-page.jsx
@@ -10,7 +10,7 @@ export default function ErrorPage() {
     <main id="error-page">
       <h1>404</h1>
       <p><span>Oups! La page que</span> vous demandez n'existe pas</p>
-      <Link to={`/`}>Retourner sur la page d’accueil</Link>
+      <Link to={`/Kasa/`}>Retourner sur la page d’accueil</Link>
     </main>
   );
 }
